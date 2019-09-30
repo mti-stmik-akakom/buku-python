a32112d14f507af2183bb64c3958d67cee702d79
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2019-09-30 07:44:20 +0700

    edit for

--- src/bab-07/for.py
+++ src/bab-07/for.py
@@ -3,21 +3,27 @@ daftar = ["first", "2nd", 3]
 for a in daftar:
     print(a)
 
+print(" === 1 === ")
+
 for a in range(20):
     print(a) # 0 - 19
 
+print(" === 2 === ")
 for a in range(1, 5):
     print(a) # 1 -4
 
-for w in 'ABCDEFG':
-    print(w)
+print(" === 3 === ")
+for a in 'ABCDEFG':
+    print(a)
 
+print(" === 4 === ")
 # range(start, stop, step)
 for a in range(1, 5, 2):
     print(a) # 1, 3
 else:
     print("bukan selisih 2")
 
+print(" === 5 === ")
 for a in range(20):
     if a > 0 and a % 2 == 0:
         print(a, " habis dibagi dua")
